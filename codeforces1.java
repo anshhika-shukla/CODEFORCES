272233277c6a27863d64a6f0555384fe381ef626
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-09 22:01:18 +0530

    Add files via upload

---
+++ codeforces1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class codeforces1      //problemA 2204 , Passing the Ball
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int c=0;
+            int n=sc.nextInt();
+            String str=sc.next();
+            for(int i=0;i<n;i++)
+            {
+                char ch=str.charAt(i);
+                if(ch=='R')
+                {
+                    c++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            System.out.println(c+1);
+        }
+    }
+}
