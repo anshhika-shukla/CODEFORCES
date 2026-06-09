0433795bef477a41b4010c9e10bdabb6e321939d
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-09 22:36:50 +0530

    Add files via upload

---
+++ codeforces5.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class codeforces5          //problem 2204,Right Maximum
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        long t=sc.nextLong();
+        while(t-->0) 
+        {
+            long n=sc.nextLong();
+            long arr[]=new long[(int)n];
+            for(int i=0;i<n;i++) 
+            {
+                arr[i]=sc.nextLong();
+            }
+            long ans=0;
+            long m=arr[0];
+            for(int i=1;i<n;i++) 
+            {
+                m=Math.max(arr[i],m);
+                if(arr[i]>=m) 
+                {
+                    ans++;
+                }
+            }
+            System.out.println((ans+1));
+        }
+    }
+}
