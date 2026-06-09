eb13f6d42c11f403d4444168a4039bb86430db31
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-09 23:49:40 +0530

    Add files via upload

---
+++ codeforces6.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class codeforces6    // problem 2153,Circle of Apple Trees
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            
+            Arrays.sort(arr);
+            
+            int cnt = 1;
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] != arr[i + 1]) {
+                    cnt++;
+                }
+            }
+            
+            System.out.println(cnt);
+        }
+        
+    }
+}
