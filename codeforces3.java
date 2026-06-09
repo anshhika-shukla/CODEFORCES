3f41a88fe20f63c09d8711df69a0665ae782d713
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-09 22:13:17 +0530

    Add files via upload

---
+++ codeforces3.java
@@ -0,0 +1,28 @@
+
+
+import java.util.Scanner;
+public class codeforces3    //contest 58 problem A, Chat room 
+{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    String s=sc.next();
+    String t="hello";
+    int j=0;
+   for (int i = 0; i < s.length() && j < t.length(); i++) 
+   {
+        if (s.charAt(i) == t.charAt(j)) 
+        {
+            j++;
+        }
+    }
+      if (j == t.length()) 
+    {
+            System.out.println("YES");
+    } else 
+    {
+            System.out.println("NO");
+    }
+    
+}
+}
