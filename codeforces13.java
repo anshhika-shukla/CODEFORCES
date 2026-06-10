24735b5c3b6bf008b6224951567b02551b45bed5
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-10 18:53:32 +0530

    Add files via upload

---
+++ codeforces13.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class codeforces13     // problem 1899,Game with Integers
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=1;i<=t;i++)
+        {
+            int n=sc.nextInt();
+            if(n%3!=0)
+            {
+                System.out.println("First");
+            }
+            else
+            {
+                System.out.println("Second");
+            }
+        }
+    }
+}
