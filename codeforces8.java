017e636d8e1283e3262ee76b5ad6e62852225090
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-10 18:26:14 +0530

    Add files via upload

---
+++ codeforces8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class codeforces8 {         //problem 4, Watermelon
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int w = scanner.nextInt();
+        
+        
+        if (w > 2 && w % 2 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
