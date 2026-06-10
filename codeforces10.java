dbcd406300be36418f8d22b1f5664eb294b654ed
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-10 18:30:23 +0530

    Add files via upload

---
+++ codeforces10.java
@@ -0,0 +1,35 @@
+
+
+import java.util.*;
+public class codeforces10 {     //problem 1512,  Spy Detected!
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            int majority;
+
+            if (a[0] == a[1] || a[0] == a[2]) {
+                majority = a[0];
+            } else {
+                majority = a[1];
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (a[i] != majority) {
+                    System.out.println(i + 1);
+                    break;
+                }
+            }
+        }
+    }
+}
