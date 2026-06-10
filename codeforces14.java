820d576536c3d7a413b08e83d60548a4e1bff8fd
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-10 18:57:24 +0530

    codeforces14.java

--- codeforces14.java
+++ codeforces14.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class codeforces14          //Problem 1806,Walking Master
+public class codeforces14          //Problem 1806, Walking Master
 {
     public static void main(String args[])
     {
