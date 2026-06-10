cf2c5e669db826897ea59e6b0494f71c6d1bcaac
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-10 18:54:02 +0530

    codeforces13.java

--- codeforces13.java
+++ codeforces13.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class codeforces13     // problem 1899,Game with Integers
+public class codeforces13     //Problem 1899,Game with Integers
 {
     public static void main(String args[])
     {
