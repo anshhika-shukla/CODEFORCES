c94c2de7550e113c8adab0154fe503f64a5dbbe0
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-10 18:42:56 +0530

    Add files via upload

---
+++ codeforces11.java
@@ -0,0 +1,19 @@
+
+
+import java.util.*;
+public class codeforces11  //problem 2210, A Simple Sequencea
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            for(int i=n;i>=1;i--)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
