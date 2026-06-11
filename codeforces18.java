4b35f04d3ee0e877f63dbdcfa8ada12d0602ac9a
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-11 21:45:17 +0530

    Add files via upload

---
+++ codeforces18.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class codeforces18             //problem 2218,The 67th Integer Problem
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) 
+        {
+            int x=sc.nextInt();
+            if(x>=0 && x<67)
+            {
+                System.out.println(x+1);
+            }
+            else if(x>=67)
+            {
+                System.out.println(67);
+            }
+            else
+            {
+                System.out.println(x);
+            }
+        }
+    }
+}
