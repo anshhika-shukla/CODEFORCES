a4475ecf089747912d51d1f0a6bc5b1ce66ed97c
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-11 22:21:22 +0530

    Add files via upload

---
+++ codeforces22.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class codeforces22           //problem 1873 , Good Kid
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n = sc.nextInt();
+            long a[] = new long[n];
+            for (int i = 0; i < n; i++) 
+            {
+                a[i] = sc.nextLong();
+            }
+            Arrays.sort(a);
+            a[0]++;
+            long product = 1;
+            for (int i = 0; i < n; i++)
+            {
+                product =product * a[i];
+            }
+            System.out.println(product);
+        }
+    }
+}
