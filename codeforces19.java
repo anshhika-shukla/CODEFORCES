4ee883040e562c6d75a68bd2e593d4106006bcfa
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-11 21:46:17 +0530

    Add files via upload

---
+++ codeforces19.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class codeforces19            //problem 2121,Letter Home
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n = sc.nextInt();
+            int s = sc.nextInt();
+            int[] x = new int[n];
+            for (int i = 0; i < n; i++) 
+            {
+                x[i] = sc.nextInt();
+            }
+            int ans = Math.min(Math.abs(s - x[0]), Math.abs(s - x[n - 1])) 
+                      + (x[n - 1] - x[0]);
+            System.out.println(ans);
+        }
+    }
+}
