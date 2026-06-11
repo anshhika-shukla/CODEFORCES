22cd6b1960682c0a5c657cce47687694b2c08fee
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-11 21:40:37 +0530

    Add files via upload

---
+++ codeforces15.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class codeforces15                     //problem 1487,Pythagorean Triples
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int l=(int)Math.sqrt((2*n)-1);
+            System.out.println((l-1)/2);
+        }
+    }
+}
