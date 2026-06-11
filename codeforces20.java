ea437f705b8bc93500e627bc33b2c1cb894eec36
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-11 21:47:49 +0530

    Add files via upload

---
+++ codeforces20.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class codeforces20                 //problem 1807,Plus or Minus
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(a+b==c)
+            {
+                System.out.println("+");
+            }
+            else
+            {
+                 System.out.println("-");
+            }
+        }
+    }
+}
