4c79f7a16d8286a60de6906b704ba62b078226a4
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-13 00:47:21 +0530

    Add files via upload

---
+++ codeforces30.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class codeforces30         //problem 705A, Hilk
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                System.out.print("I hate");
+            } else {
+                System.out.print("I love");
+            }
+
+            if (i < n) {
+                System.out.print(" that ");
+            } else {
+                System.out.print(" it");
+            }
+        }
+    }
+}
