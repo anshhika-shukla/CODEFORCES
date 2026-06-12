83a603fb1082170d28cba269ac9a4f2484335ccb
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-12 23:50:32 +0530

    Add files via upload

---
+++ codeforces24.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class codeforces24     //problem 731A, Night at the Museum
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char cur = 'a';
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) 
+        {
+            char c = s.charAt(i);
+            int d = Math.abs(c - cur);
+            ans = ans + Math.min(d, 26 - d);
+            cur = c;
+        }
+        System.out.println(ans);
+    }
+}
