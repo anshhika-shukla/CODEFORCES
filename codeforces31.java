4a077009635dd70370934c211515b7e5f25371a2
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-14 23:35:46 +0530

    Add files via upload

---
+++ codeforces31.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class codeforces31   //problem 2093,Ideal Generator
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int k = sc.nextInt();
+            if (k % 2 != 0)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
