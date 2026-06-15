472ff76afa088679bee47e4319670777e66bb495
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-15 14:46:51 +0530

    codeforces40.java

--- codeforces40.java
+++ codeforces40.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class codeforces40       //problem 337,A. Puzzles
+public class codeforces40       //Problem 337,A. Puzzles
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
