26f6c7f52688d06ca04f4c6691875bfdbddee027
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-15 14:43:08 +0530

    Add files via upload

---
+++ codeforces40.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class codeforces40       //problem 337,A. Puzzles
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] f = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            f[i] = sc.nextInt();
+        }
+
+    
+        Arrays.sort(f);
+
+        int ans = Integer.MAX_VALUE;
+
+    
+             for (int i = 0; i + n - 1 < m; i++) {
+            int B = f[i];
+            int A = f[i + n - 1];
+            ans = Math.min(ans, A - B);
+        }
+
+        System.out.println(ans);
+    }
+}
