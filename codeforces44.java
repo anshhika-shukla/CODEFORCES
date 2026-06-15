ef22cb6b97d0a217dca1fb7178d37164ddb73154
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-15 14:44:23 +0530

    Add files via upload

---
+++ codeforces44.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class codeforces44         //problem 12,A. Super Agent
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[][] v = new char[3][3];
+
+        
+        for (int i = 0; i < 3; i++) {
+String s=sc.next();
+            for (int j = 0; j < 3; j++) {
+                v[i][j] = s.charAt(j);
+            }
+        }
+
+        
+        if ((v[0][0] == v[2][2]) &&
+            (v[0][1] == v[2][1]) &&
+            (v[0][2] == v[2][0]) &&
+            (v[1][0] == v[1][2])) {
+
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close();
+    }
+}
