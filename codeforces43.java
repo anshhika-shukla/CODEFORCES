d2b342d5d2fdb8eb5637a2c48208f5eb2c0dddee
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-15 14:44:05 +0530

    Add files via upload

---
+++ codeforces43.java
@@ -0,0 +1,32 @@
+
+
+import java.util.*;
+public class codeforces43             //problem 2227,B. Party Monster
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            String s=sc.next();
+            int count=0;            
+            for(int i=0;i<n;i++)
+            {
+                if(s.charAt(i)=='(')
+                {
+                    count++;
+                }
+            }
+            if(count == n-count)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
