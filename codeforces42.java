9338c2e3ba7b85ee49a24b839f75ca0a2949e355
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-15 14:43:46 +0530

    Add files via upload

---
+++ codeforces42.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class codeforces42         //problem 2227,A. Koshary
+{
+    public static void main(String args[]) 
+{
+        Scanner sc = new Scanner(System.in);
+        long t = sc.nextLong();
+        while (t-- > 0)
+        {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            if (a % 2 != 0 && b % 2 != 0)
+             System.out.println("NO");
+            else
+                System.out.println("YES");
+        }
+    }
+}
