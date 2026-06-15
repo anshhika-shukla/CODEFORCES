cc002f97aeb34e909a208967aaf9c9012288664e
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-15 14:37:34 +0530

    Add files via upload

---
+++ codeforces33.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+public class codeforces33       // problem 1081,Definite Game
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int v=sc.nextInt();
+        if(v==2) 
+        {
+            System.out.println(2);
+            return;
+        }
+        System.out.println(1);
+    }
+}
