613330b1063b9e3a523006af16d169f83b65de2d
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:46:09 +0530

    codeforces47.java

--- codeforces47.java
+++ codeforces47.java
@@ -1,6 +1,6 @@
 
 import java.util.*;
-public class codeforces47         // problem 1505,A. Is it rated - 2
+public class codeforces47         // Problem 1505,A. Is it rated - 2
 {
     public static void main(String args[]) 
     {
