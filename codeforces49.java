e293b3b2b6d67d3a1a2573f056931c59068af898
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:46:47 +0530

    codeforces49.java

--- codeforces49.java
+++ codeforces49.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class codeforces49      //problem 2148,A. Sublime Sequence
+public class codeforces49      //Problem 2148,A. Sublime Sequence
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
