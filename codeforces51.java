f286793416458306d899e858cb2f18cd5cbfbaa5
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:55:17 +0530

    Add files via upload

---
+++ codeforces51.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class codeforces51    //problem 2140,A. Shift Sort
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+
+            int c1 = 0;
+
+            for (char c : s.toCharArray()) {
+                if (c == '1') {
+                    c1++;
+                }
+            }
+
+            int z = 0;
+            int o = n - c1;
+
+            for (int i = o; i < n; i++) {
+                if (s.charAt(i) == '0') {
+                    z++;
+                }
+            }
+
+            System.out.println(z);
+        }
+
+
+    }
+}
