662558476a53b7883bb1c4ccb9212454ebe291f0
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:44:58 +0530

    Add files via upload

---
+++ codeforces49.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class codeforces49      //problem 2148,A. Sublime Sequence
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t;
+        t = sc.nextInt();
+
+        while (t-- > 0) {
+            int x, n;
+            x = sc.nextInt();
+            n = sc.nextInt();
+
+            if (n % 2 == 0) {
+                System.out.println("0");
+            } else {
+                System.out.println(x);
+            }
+        }
+
+        sc.close();
+    }
+}
