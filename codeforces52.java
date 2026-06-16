09edfc4155dbd57c9dcb4832327be3fb7ab793e8
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:55:38 +0530

    Add files via upload

---
+++ codeforces52.java
@@ -0,0 +1,37 @@
+
+
+import java.util.*;
+public class codeforces52      //problem 2200,A. Eating Game
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) 
+        {
+            int n=sc.nextInt();
+            int a[]=new int[n];
+            for(int i=0;i<n;i++) 
+            {
+                a[i]=sc.nextInt();
+            }
+            int mx=a[0];
+            for(int i=1;i<n;i++) 
+            {
+                if(a[i]>mx) 
+                {
+                    mx=a[i];
+                }
+            }
+            int cnt=0;
+            for(int i=0;i<n;i++) 
+            {
+                if(a[i]==mx) 
+                {
+                    cnt++;
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
+}
