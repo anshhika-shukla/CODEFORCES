36a0549a7bfba55f6fc9d0bd4d51694cf8a4b846
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:56:36 +0530

    codeforces51.java

--- codeforces51.java
+++ codeforces51.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class codeforces51    //problem 2140,A. Shift Sort
+public class codeforces51    //Problem 2140,A. Shift Sort
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
