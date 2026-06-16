6c298f2401e1d1bd1b1fc3d4b1aea64cd1b84d6c
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:56:08 +0530

    Add files via upload

---
+++ codeforces53.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class codeforces53      //problem
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        long t=sc.nextLong();
+        int n=sc.nextInt();
+        while(t-->0)
+        {
+            if(n<=100)
+            
+                System.out.println(n-1);
+            
+        }
+    }
+}
