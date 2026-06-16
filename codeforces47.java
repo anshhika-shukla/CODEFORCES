d641b5de9f00479bc55f81fbefde998cad6b6392
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:44:22 +0530

    Add files via upload

---
+++ codeforces47.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class codeforces47         // problem 1505,A. Is it rated - 2
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        while (sc.hasNextLine()) 
+        {
+            String s=sc.nextLine();
+            System.out.println("NO");
+        }
+    }
+}
