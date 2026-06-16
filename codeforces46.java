97308e09b094feb7d0ad799bce24e199a67b14fa
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:45:48 +0530

    codeforces46.java

--- codeforces46.java
+++ codeforces46.java
@@ -1,7 +1,7 @@
 
 
 import java.util.*;
-public class codeforces46        //problem 2091,C. Combination Lock
+public class codeforces46        //Problem 2091,C. Combination Lock
 {
     public static void main(String args[]) 
     {
