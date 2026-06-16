e5574bf93598980a61051aafdacbe7e7ccb03af9
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-16 23:44:04 +0530

    Add files via upload

---
+++ codeforces46.java
@@ -0,0 +1,31 @@
+
+
+import java.util.*;
+public class codeforces46        //problem 2091,C. Combination Lock
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) 
+        {
+            int n=sc.nextInt();
+            if (n%2==0) 
+            {
+                System.out.println("-1");
+            } 
+            else 
+            {
+                for(int i=2;i<=n;i=i+2) 
+                {
+                    System.out.print(i+" ");
+                }
+                for(int i=1;i<=n;i=i+2) 
+                {
+                    System.out.print(i+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
