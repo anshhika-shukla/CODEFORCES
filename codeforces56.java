361829d351804225eed9304f6cb5df363b6160ef
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-17 23:11:32 +0530

    Add files via upload

---
+++ codeforces56.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class codeforces56              //problem 1720,A. Burenka Plays with Fractions
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+            long d = sc.nextLong();
+
+            long x = a * d;
+            long y = b * c;
+
+            if (x == y) {
+                System.out.println(0);
+            } 
+            else if (a == 0 || c == 0) {
+                System.out.println(1);
+            } 
+            else if (x % y == 0 || y % x == 0) {
+                System.out.println(1);
+            } 
+            else {
+                System.out.println(2);
+            }
+        }
+
+    
+    }
+}
