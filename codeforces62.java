10c716997f517bf65f702f14b3ef39b8432cf38c
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-18 22:07:53 +0530

    Add files via upload

---
+++ codeforces62.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class codeforces62     //problem 514,A. Chewbaсca and Number
+{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int a = arr[i] - '0';
+            int b = 9 - a;
+
+            if (b < a) {
+
+                if (i == 0 && b == 0) {
+                    continue;
+                } else {
+                    arr[i] = (char) (b + '0');
+                }
+            }
+        }
+
+        System.out.print(new String(arr));
+    }
+}
