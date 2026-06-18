299e348013bc1fb8cefae76c6f7552264b5f7440
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-18 22:08:29 +0530

    Add files via upload

---
+++ codeforces64.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class codeforces64    //problem 1371,A. Magical Sticks
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0)
+        {
+            long n = sc.nextLong();
+            System.out.println((n + 1) / 2);
+        }
+    }
+}
