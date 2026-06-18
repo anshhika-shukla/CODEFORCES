4fedd160f8379c7add6271db94463418fa4b9e31
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-18 22:07:34 +0530

    Add files via upload

---
+++ codeforces61.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class codeforces61 //problem 1285,A. Mezo Playing Zoma
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String s = sc.next();
+
+        System.out.println(n + 1);
+    }
+}
