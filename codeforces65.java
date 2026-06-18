635cbdd9de087e4a70be70efcc03963a28ec7847
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-18 22:09:00 +0530

    Add files via upload

---
+++ codeforces65.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+public class codeforces65   //problem 1807,B. Grab the Candies
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i]=sc.nextInt();
+            }
+            int e_Count=0;
+            int o_Count=0;
+            for(int i=0;i<n;i++)
+            {
+                if(arr[i]%2==0)
+                {
+                    e_Count+=arr[i];
+                }
+                else
+                {
+                    o_Count+=arr[i];
+                }
+            }
+            if(e_Count>o_Count)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
