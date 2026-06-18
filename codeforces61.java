fc1877bf196e9d5a25969765dffc8f6fd9cbdfa1
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-18 22:09:57 +0530

    codeforces61.java

--- codeforces61.java
+++ codeforces61.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class codeforces61 //problem 1285,A. Mezo Playing Zoma
+public class codeforces61 //Problem 1285,A. Mezo Playing Zoma
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
