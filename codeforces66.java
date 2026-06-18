0097ee65c02e1becba6a8f3442b54ce11ab60616
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-18 22:09:22 +0530

    Add files via upload

---
+++ codeforces66.java
@@ -0,0 +1,27 @@
+
+
+import java.util.*;
+public class codeforces66   //problem 1360,B. Honest Coach
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) 
+            {
+                a[i] = sc.nextInt();
+            }
+            Arrays.sort(a);
+            int ans = (int) 1e9;
+            for (int i = 1; i < n; i++) 
+            {
+                ans = Math.min(ans, a[i] - a[i - 1]);
+            }
+            System.out.println(ans);
+        }
+    }
+}
