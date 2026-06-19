e442c1a83bf801fefc3b7e3979143a45ff7bff4c
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-19 22:02:26 +0530

    Add files via upload

---
+++ codeforces98.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class codeforces98    //problem 959,A. Mahmoud and Ehab and the even-odd game
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        if(n%2==0)
+        {
+            System.out.println("Mahmoud");
+        }
+        else
+        {
+            System.out.println("Ehab");
+        }
+    }
+}
