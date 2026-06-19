e0ae0d48fc7ad481a184c0f6a524356c96f9983b
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-19 22:04:49 +0530

    codeforces72.java

--- codeforces72.java
+++ codeforces72.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class codeforces72   //problem 1593,A. Elections
+public class codeforces72   //Problem 1593,A. Elections
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
