c88f805d04250ce3f659e78d1e1eb66aba4642f0
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-19 21:59:41 +0530

    Add files via upload

---
+++ codeforces91.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class codeforces91           //problem 2029,A. Set
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=1;i<=t;i++)
+        {
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            int k = sc.nextInt();   
+            int ans=((r/k)-l+1);
+            if(ans<0)
+            {
+                System.out.println("0");
+            }
+            else
+            {
+                System.out.println(ans);
+            }
+        }
+    }
+}
