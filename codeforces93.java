34dbeecf158ff271fb6313f56612d2d75c86dd08
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-19 22:00:26 +0530

    Add files via upload

---
+++ codeforces93.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class codeforces93     //problem 1760,A. Medium Number
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(a>b && a>c)
+            {
+                if(b>c)
+                {
+                    System.out.println(b);
+                }
+                else
+                {
+                    System.out.println(c);
+                }
+            }
+            if(b>a && b>c)
+            {
+                if(a>c)
+                {
+                    System.out.println(a);
+                }
+                else
+                {
+                    System.out.println(c);
+                }
+            }
+            if(c>a && c>b)
+            {
+                if(a>b)
+                {
+                    System.out.println(a);
+                }
+                else
+                {
+                    System.out.println(b);
+                }
+            }
+        }
+    }
+}
