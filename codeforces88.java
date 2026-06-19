46ebb2c39dfaaf2c0d60e7b33766a0254c5954e9
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-19 21:57:33 +0530

    Add files via upload

---
+++ codeforces88.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class codeforces88      //problem 2234,B. Palindrome, Twelve and Two Terms
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        long t=sc.nextLong();
+        while(t-->0) 
+        {
+            long n=sc.nextLong();
+            long ans;
+            long a=n%12;
+            if(a==10)
+            {
+                ans=22;
+            }
+            else
+            {
+                ans=a;
+            }
+            if(ans<=n)
+            {
+                System.out.println(ans+" " +(n - ans));
+            }
+            else
+            {
+                System.out.println(-1);
+            }
+        }
+    }
+}
