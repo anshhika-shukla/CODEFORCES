db44c9b3ff7d0c46fcb1298382052ddff5a98970
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-19 21:45:12 +0530

    Add files via upload

---
+++ codeforces67.java
@@ -0,0 +1,18 @@
+
+
+import java.util.*;
+public class codeforces67    //problem 2065,A. Skibidus and Amog'u
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            String s=sc.next();
+            s=s.substring(0,s.length()-2);
+            s=s+"i";
+            System.out.println(s);
+        }
+    }
+}
