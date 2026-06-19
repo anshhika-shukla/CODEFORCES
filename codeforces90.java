e1398cfd5d67145dc1757271a69e0aa36f6d43ba
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-19 21:59:22 +0530

    Add files via upload

---
+++ codeforces90.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class codeforces90         //problem 1514,B. AND 0, Sum Big
+{
+    static final long MOD = 1000000007L;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+
+            long ans = 1;
+
+            for (long i = 1; i <= k; i++) {
+                ans = (ans * n) % MOD;
+            }
+
+            System.out.println(ans);
+        }
+
+        sc.close();
+    }
+}
