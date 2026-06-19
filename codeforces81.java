1dc229df8f18f08a0b4bc0844f9d8f556861019a
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-19 21:54:33 +0530

    Add files via upload

---
+++ codeforces81.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+public class codeforces81     //problem 1842,B. Tenzing and Books
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) 
+        {
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            int k=0;
+            for(int i=0;i<3;i++) 
+            {
+                boolean stop=false;
+                for(int j=0;j<n;j++) 
+                {
+                    int v=sc.nextInt();
+                    if(!stop) 
+                    {
+                        if((v|x)!=x) 
+                        {
+                            stop = true;
+                        }
+                        else
+                        {
+                            k=k|v;
+                        }
+                    }
+                }
+            }
+            if(k==x)
+            {
+                System.out.println("Yes");
+            }
+            else
+            {
+                System.out.println("No");
+            }
+        }
+    }
+}
