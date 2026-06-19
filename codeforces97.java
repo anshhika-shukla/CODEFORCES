57fe3b935a2d07733b207f840283a4180f301b5e
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-19 22:02:08 +0530

    Add files via upload

---
+++ codeforces97.java
@@ -0,0 +1,29 @@
+
+
+import java.util.*;
+public class codeforces97         //problem 1742,B. Increasing
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) 
+        {
+            int n=sc.nextInt();
+            HashSet<Integer> s=new HashSet<>();
+            for(int i=0;i<n;i++) 
+            {
+                int x=sc.nextInt();
+                s.add(x);
+            }
+            if(s.size()==n) 
+            {
+                System.out.println("YES");
+            } 
+            else 
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
