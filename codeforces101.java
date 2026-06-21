3cf560cd3d49914fbcbfe45fc88c2ca66e007731
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-21 22:55:25 +0530

    Add files via upload

---
+++ codeforces101.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class codeforces101     //problem 2155,A. El fucho
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            int a=sc.nextInt();
+            System.out.println(2*a-2);
+        }
+    }
+}
