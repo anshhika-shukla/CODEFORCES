b1e584a9f7d49f67a96176d14a3b31fb3227e34f
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-25 21:20:31 +0530

    Update codeforces106.java
    Refactored code to solve Problem 2056A. Shape Perimeter, replacing previous logic with new input handling and calculation.

--- codeforces106.java
+++ codeforces106.java
@@ -1,30 +1,30 @@
+
 import java.util.*;
-public class codeforces106
+public class codeforces106 
 {
-    public static void main(String args[])
+    public static void main(String args[])            // Problem 2056A. Shape Perimeter
     {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
-        for(int i=1;i<=t;i++)
+        while(t-->0) 
         {
             int n=sc.nextInt();
-            int a[]=new int[n];
-            for(int j=0;j<n;j++)
+            int m=sc.nextInt();
+            int x[]=new int[n];
+            int y[]=new int[n];
+            for(int i=0;i<n;i++) 
             {
-                a[j]=sc.nextInt();
+                x[i]=sc.nextInt();
+                y[i]=sc.nextInt();
             }
-            int c=0;
-            for(int j=0;j<n-1;j++)
+            long sum=0;
+            for(int i=1;i<n;i++) 
             {
-                if(a[j]%2 == a[j+1]%2)
-                {
-                    a[j]=a[j]*a[j+1];
-                    c++;
-                }
+                x[i]=m-x[i];
+                y[i]=m-y[i];
+                sum=sum+2L*(x[i]+y[i]);
             }
-            System.out.println(c);
+            System.out.println((4L*m*n)-sum);
         }
     }
 }
-
- 
