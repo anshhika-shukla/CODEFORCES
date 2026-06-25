b863688a332141aaa35add5b0a685e9202a3382d
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-25 22:08:31 +0530

    Update codeforces107.java

--- codeforces107.java
+++ codeforces107.java
@@ -1,40 +1,21 @@
 import java.util.*;
+
 public class codeforces107
 {
-    public static void main(String args[])
+    public static void main(String args[])                   // Problem 2094A. Trippi Troppi
     {
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        for(int i=1;i<=t;i++)
-        {
-            int n=sc.nextInt();
-            int ans=0;
-            if(n<10) 
-            {
-                ans=n;
-            }
-            else if(n<100)
-            {
-                ans=9+n/10;
-            }
-            else if(n<1000)
-            { 
-                ans=18+n/100;
-            }
-            else if(n<10000)
-            {
-                ans=27+n/1000;
-            }
-            else if(n<100000) 
-            {
-                ans=36+n/10000;
-            }
-            else 
-            {
-                ans=45+n/100000;
-            }
-            System.out.println(ans);
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String a = sc.next();
+            String b = sc.next();
+            String c = sc.next();
+
+            System.out.println("" + a.charAt(0) + b.charAt(0) + c.charAt(0));
         }
+
+        
     }
 }
-           
