b83020ca0df60f9f695735f4a40ac0f7f4f6f0ee
Anshika Shukla <shuklaanshika145@gmail.com>
2026-06-26 23:48:04 +0530

    Add files via upload

---
+++ codeforces108.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class codeforces108    //problem 1980,A. Problem Generator
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            String s = sc.next();
+            int[] freq = new int[7];
+            for (int i = 0; i < s.length(); i++) 
+            {
+                char ch = s.charAt(i);
+                freq[ch - 'A']++;
+            }
+
+            int ans = 0;
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (freq[i] < m)
+                {
+                    ans += (m - freq[i]);
+                }
+            }
+
+            System.out.println(ans);
+        }
+
+    }
+}
