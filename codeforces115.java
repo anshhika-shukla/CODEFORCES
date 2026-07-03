cace2d5ca4cc0afbea91f3accbd935daab23673b
Anshika Shukla <shuklaanshika145@gmail.com>
2026-07-03 23:06:04 +0530

    Add files via upload

---
+++ codeforces115.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class codeforces115  //problem 1927,A. Make it White
+ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+
+            int first = -1;
+            int last = -1;
+
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == 'B') {
+                    if (first == -1) {
+                        first = i;
+                    }
+                    last = i;
+                }
+            }
+
+            System.out.println(last - first + 1);
+        }
+
+    }
+}
