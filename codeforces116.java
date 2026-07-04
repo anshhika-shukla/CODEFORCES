0cd1cb4df1cb4c7cf3b8f451c159a4e1919dd090
Anshika Shukla <shuklaanshika145@gmail.com>
2026-07-04 22:58:26 +0530

    codeforces116.java

--- codeforces116.java
+++ codeforces116.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class codeforces116   //problem 2171,A. Shizuku Hoshikawa and Farm Legs
+public class codeforces116   //Problem 2171,A. Shizuku Hoshikawa and Farm Legs
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
