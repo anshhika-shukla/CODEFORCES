1c5fc858683290eae7654c1ffc392270badf7abc
Anshika Shukla <shuklaanshika145@gmail.com>
2026-07-06 23:27:03 +0530

    Add files via upload

---
+++ codeforces118.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+public class codeforces118  //problem 584,A. Olesya and Rodion
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int t=sc.nextInt();
+        if(t==10) 
+        {
+            if(n==1) 
+            {
+                System.out.println(-1);
+            } 
+            else 
+            {
+                System.out.print(1);
+                for(int i=1;i<n;i++) 
+                {
+                    System.out.print(0);
+                }
+            }
+        } 
+        else 
+        {
+            for(int i=0;i<n;i++) 
+            {
+                System.out.print(t);
+            }
+        }
+    }
+}
